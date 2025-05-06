4f401c0f2908633ffa357d0d3502d53669bfcef7
trustdan <danieltuckerrust@gmail.com>
2025-05-06 06:20:56 -0500

    updated requirements.txt to use numpy>=1.20.0,<2.0.0

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     install_requires=[
         "ib_insync>=0.9.70",
         "pandas>=1.3.0",
-        "numpy>=1.20.0",
+        "numpy>=1.20.0,<2.0.0",
         "pytz>=2021.1",
         "requests>=2.25.0",
         "lxml>=4.6.3",
